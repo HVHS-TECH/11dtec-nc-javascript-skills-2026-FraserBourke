de6845590c1f71f7f47a6265d7aa0dc56047b75f
FraserBourke <24347fb@hvhs.school.nz>
2026-04-29 02:16:57 +0000

    Apr 29, 2026, 2:16 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -3,7 +3,18 @@ This is a block comment.
 Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("Hello")
+
+//variables
+
+/************************ 
+Main code
+************************/
+
+
+/************************ 
+Functions
+************************/
 
 // This is a single line comment
 
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,26 @@
 /* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Auzio is my king")
+
+//variables
+let username = "Fraser"
+let year = 2026;
+let age = "15";
+let money = 50;
+
+
+/************************ 
+Main code
+************************/
+
+
+/************************ 
+Functions
+************************/
+
+// This is a single line comment
+
