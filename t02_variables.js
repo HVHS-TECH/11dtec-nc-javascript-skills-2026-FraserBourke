e9ceb660634e2d33e2163067264ed6fcbaa7671b
FraserBourke <24347fb@hvhs.school.nz>
2026-04-29 02:23:19 +0000

    Apr 29, 2026, 2:23 AM

--- t02_variables.js
+++ t02_variables.js
@@ -7,7 +7,7 @@ console.log("Running t01_introduction.js")
 console.log("Auzio is my king")
 
 //variables
-let username = "Fraser"
+letUsername = "Fraser"
 let year = 2026;
 let age = "15";
 let money = 50;
@@ -16,7 +16,7 @@ let money = 50;
 /************************ 
 Main code
 ************************/
-
+console.log("Hi "+ Username + " as of")
 
 /************************ 
 Functions
