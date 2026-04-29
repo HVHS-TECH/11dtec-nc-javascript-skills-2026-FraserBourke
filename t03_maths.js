8535e2e09cf04ed57b9add371ca0958a311ad9d2
FraserBourke <24347fb@hvhs.school.nz>
2026-04-29 02:43:53 +0000

    Apr 29, 2026, 2:43 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,27 @@
 /* Paste your code from task 2 here */
+/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Auzio is my king")
+
+//variables
+let Username = "Fraser";
+let year = 2026;
+let age = "15";
+let money = 50;
+
+
+/************************ 
+Main code
+************************/
+console.log("Hi "+ Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars ");
+
+/************************ 
+Functions
+************************/
+
+// This is a single line comment
+
