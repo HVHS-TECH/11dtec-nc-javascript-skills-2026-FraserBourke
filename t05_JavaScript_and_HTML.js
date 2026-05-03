56eee75da9079f612f274716cde31291fe6c4bcc
FraserBourke <24347fb@hvhs.school.nz>
2026-05-03 22:24:10 +0000

    May 3, 2026, 10:24 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,36 @@
 /* Paste your code from task 3, Maths here */
+/* Paste your code from task 2 here */
+/* Paste your code from task 1 here */
+/****************************
+Name of task: Maths
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Auzio is my king")
+
+//variables
+let Username = "Fraser";
+let year = 2026;
+let age = "15";
+let money = 20;
+let birthYear;
+let oldAge;
+oldAge = age + 10;
+birthYear = year - age
+halfMoney = money / 2
+
+/************************ 
+Main code
+************************/
+console.log("Hi "+ Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars ");
+console.log("You were born in " + birthYear)
+console.log("In 10 years you will be " + oldAge + " years old ")
+console.log("you have " + money + " dollars ")
+console.log("You spend half of your money, now you have " + halfMoney)
+console.log(" Then you get $3, now you have " + (halfMoney + 3) + " dollars ")
+
+/************************ 
+Functions
+************************/
+
+// This is a single line comment
+
