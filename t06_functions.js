a713cceca80ec67147bf41737ea4499ab970fc98
FraserBourke <24347fb@hvhs.school.nz>
2026-05-04 23:48:42 +0000

    May 4, 2026, 11:48 PM

--- t06_functions.js
+++ t06_functions.js
@@ -34,8 +34,8 @@ OUTPUT.innerHTML += (" <p>Then you get $3, now you have " + (halfMoney + 3) + " 
 Functions
 ************************/
 function writeLine(){
+OUTPUT.innerHTML += "<p>Less code matters</p>";
 
-    
 }
 // This is a single line comment
 
