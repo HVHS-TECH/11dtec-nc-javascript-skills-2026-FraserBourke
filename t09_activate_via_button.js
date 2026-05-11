1a5cd71e16907850ad94e38767e5669181797ed9
FraserBourke <24347fb@hvhs.school.nz>
2026-05-11 23:32:59 +0000

    May 11, 2026, 11:32 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,60 @@
 /* Paste your code from task 7 here*/
+/****************************
+Name of task: activate via button
+****************************/
+console.log("Running t05_introduction.js")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+function start() {
+
+//variables
+let Username = "Fraser";
+let year = 2026;
+let age = "15";
+let money = 20;
+let birthYear;
+let oldAge;
+let name = "Fraser";
+let currentYear = 2026;
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+const OUTPUT = document .getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+/************************ 
+Main code
+************************/
+
+OUTPUT.innerHTML += ("<p>Hi "+ Username + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old </p>")
+OUTPUT.innerHTML += ("<p>you have " + money + " dollars </p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += (" <p>Then you get $3, now you have " + (halfMoney + 3) + " dollars </p>")
+
+/************************ 
+Functions
+************************/
+function start() {
+spaceForJavaScriptOutput.innerHTML
+spaceForJavaScriptOutput.innerHTML
+answer = currentYear - userAge;
+spaceForJavaScriptOutput.innerHTML
+}
+
+
+function Welcome(){
+OUTPUT.innerHTML += "<p>Welcome</p>";
+
+}
+}
+Welcome();
+
+function displayWelcome(_name, _age){
+OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+
+displayWelcome("Ms Alice", 16);
+displayWelcome("Mr Bob", 67);
+// This is a single line comment
+
