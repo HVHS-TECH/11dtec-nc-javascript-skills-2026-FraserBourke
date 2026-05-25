8901ead6ee423e4becf0945e5525d8a2f370c605
FraserBourke <24347fb@hvhs.school.nz>
2026-05-25 23:55:44 +0000

    May 25, 2026, 11:55 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,68 @@
 /* Paste your code from the last task */
+/* Paste your code from the last task */
+/****************************
+Name of task: Data_types
+****************************/
+
+console.log("Running t05_introduction.js")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+function start() {
+
+//variables
+let Username = "Fraser";
+let year = 2026;
+let age = "15";
+let money = 20;
+let birthYear;
+let oldAge;
+let name = "Fraser";
+let currentYear = 2026;
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+const OUTPUT = document .getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+/************************ 
+Main code
+************************/
+
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+
+OUTPUT.innerHTML += ("<p>Hi "+ Username + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old </p>")
+OUTPUT.innerHTML += ("<p>you have " + money + " dollars </p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += (" <p>Then you get $3, now you have " + (halfMoney + 3) + " dollars </p>")
+
+/************************ 
+Functions
+************************/
+
+function getFormInput(){
+}
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+
+function start() {
+spaceForJavaScriptOutput.innerHTML
+spaceForJavaScriptOutput.innerHTML
+answer = currentYear - userAge;
+spaceForJavaScriptOutput.innerHTML
+}
+
+
+function Welcome(){
+OUTPUT.innerHTML += "<p>Welcome</p>";
+
+}
+}
+Welcome();
+
+
+
+
+// This is a single line comment
+
