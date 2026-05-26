defac77b111df04ffa91926b50e6e829541ca1e4
FraserBourke <24347fb@hvhs.school.nz>
2026-05-26 00:20:47 +0000

    May 26, 2026, 12:20 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -44,9 +44,6 @@ Functions
 
 function getFormInput(){
 }
-const NAME_FIELD = document.getElementById("nameField");
-let userName = NAME_FIELD.value;
-OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
 
 function start() {
 spaceForJavaScriptOutput.innerHTML
