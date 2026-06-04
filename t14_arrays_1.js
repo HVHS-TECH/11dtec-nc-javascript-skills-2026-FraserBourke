7a0dafcfb3a3c0b9f404478dc28b001959fc3238
FraserBourke <24347fb@hvhs.school.nz>
2026-06-04 21:07:46 +0000

    Jun 4, 2026, 9:07 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,1 +1,79 @@
 /* Paste your code from the last task */
+/****************************
+Name of task: Data_types
+****************************/
+
+console.log("Running t05_introduction.js")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+function start() {
+
+//variables
+let Username = "Fraser";
+let year = 2026;
+let age = 15;
+let money = 20;
+let birthYear;
+let oldAge;
+let name = "Fraser";
+let currentYear = 2026;
+oldAge = age + 10;
+
+
+birthYear = year - age;
+halfMoney = money / 2;
+const OUTPUT = document .getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+/************************ 
+Main code
+************************/
+
+
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+
+OUTPUT.innerHTML += ("<p>Hi "+ Username + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old </p>")
+OUTPUT.innerHTML += ("<p>you have " + money + " dollars </p>")
+OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
+OUTPUT.innerHTML += (" <p>Then you get $3, now you have " + (halfMoney + 3) + " dollars </p>")
+
+/************************ 
+Functions
+************************/  
+let classArray = ["Alice", "Bob", "Kaia", "Olivia", "Aroha", "Josh"];
+
+let choice = CHOOSE_FIELD.value;
+
+OUTPUT.innerHTML = "You chose: " + classArray[choice] + "<br>";
+
+function getFormInput(){
+}
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+
+function start() {
+spaceForJavaScriptOutput.innerHTML
+spaceForJavaScriptOutput.innerHTML
+answer = currentYear - userAge;
+spaceForJavaScriptOutput.innerHTML
+if (age < 70){
+    OUTPUT.innerHTML += "<p>You're too young</p>";
+}
+
+}
+
+
+function Welcome(){
+OUTPUT.innerHTML += "<p>Welcome</p>";
+
+}
+}
+Welcome();
+start();
+
+
+
+// This is a single line comment
+
