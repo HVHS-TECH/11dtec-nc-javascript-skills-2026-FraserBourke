433e6089d1c10674a632485f9c05e6cece69b2d9
FraserBourke <24347fb@hvhs.school.nz>
2026-06-11 21:38:48 +0000

    Jun 11, 2026, 9:38 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -23,9 +23,7 @@ OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
 Main code
 ************************/
 
-OUTPUT.innerHTML += ("<p>Hi "+ Username + " as of " + year + " you are " + age + " years old.</p>");
-OUTPUT.innerHTML += ("<p>You were born in " + birthYear)
-OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old </p>")
+
 OUTPUT.innerHTML += ("<p>you have " + money + " dollars </p>")
 OUTPUT.innerHTML += ("<p>You spend half of your money, now you have " + halfMoney + " dollars</p>")
 OUTPUT.innerHTML += (" <p>Then you get $3, now you have " + (halfMoney + 3) + " dollars </p>")
